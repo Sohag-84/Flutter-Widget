2ad4da8c4e3ce3127dedaa46c098e446f8019eb7
sohag-84 <injamulhaqsohag19977@gmail.com>
2022-12-25 21:05:52 +0600

    create two button

--- responsive_login_ui/lib/views/login_screen.dart
+++ responsive_login_ui/lib/views/login_screen.dart
@@ -1,10 +1,43 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:responsive_login_ui/widgets/social_button.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset("assets/images/signin_balls.png"),
+              Text(
+                "Sign in",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 50,
+                ),
+              ),
+              SizedBox(height: 50),
+              SocialButton(
+                iconPath: "assets/svgs/g_logo.svg",
+                label: "Continue with Google",
+              ),
+              SizedBox(height: 20),
+              SocialButton(
+                iconPath: "assets/svgs/f_logo.svg",
+                label: "Continue with Facebook",
+                horizontalPadding: 90,
+              ),
+              SizedBox(height: 15),
+              Text("OR", style: TextStyle(fontSize: 17)),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ responsive_login_ui/lib/widgets/social_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:responsive_login_ui/constant/colors.dart';
+
+class SocialButton extends StatelessWidget {
+  final String iconPath;
+  final String label;
+  final double horizontalPadding;
+  const SocialButton({
+    Key? key,
+    required this.iconPath,
+    required this.label,
+    this.horizontalPadding = 100,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton.icon(
+      onPressed: () {},
+      icon: SvgPicture.asset(iconPath, width: 25, color: whiteColor),
+      label: Text(
+        label,
+        style: const TextStyle(fontSize: 17, color: whiteColor),
+      ),
+      style: TextButton.styleFrom(
+        padding:
+            EdgeInsets.symmetric(vertical: 30, horizontal: horizontalPadding),
+        shape: RoundedRectangleBorder(
+          side: const BorderSide(color: borderColor, width: 2),
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+  }
+}
